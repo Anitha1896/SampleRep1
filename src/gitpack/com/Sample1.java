d338c092449a8584d55a8940e6582b6abf0b0c55
Anitha1896 <anitha1896@gmail.com>
2022-01-25 13:36:23 +0530

    Commited

--- src/gitpack/com/Sample1.java
+++ src/gitpack/com/Sample1.java
@@ -3,5 +3,7 @@ package gitpack.com;
 public class Sample1 {
 public static void main(String[] args) {
 	System.out.println("GIT");
+	System.out.println("Repository");
+	System.out.println("committed");
 }
 }
