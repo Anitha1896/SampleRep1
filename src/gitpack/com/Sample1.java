f42b283e9d64eb7f8e5432f9a8bc79a3f43026bf
Anitha1896 <anitha1896@gmail.com>
2022-01-27 12:56:16 +0530

    Anitha added her code

--- src/gitpack/com/Sample1.java
+++ src/gitpack/com/Sample1.java
@@ -1,9 +1,14 @@
 package gitpack.com;
 
 public class Sample1 {
+	private void gitm1() {
+		System.out.println("committed");
+	}
+
 public static void main(String[] args) {
 	System.out.println("GIT");
 	System.out.println("Repository");
-	System.out.println("committed");
+	Sample1 s= new Sample1();
+	s.gitm1();
 }
 }
